9899381068b2f479e8ff74da104424fc9ea4fe33
Jefferson Nascimento <jf.nts10@gmail.com>
2020-04-05 23:32:22 -0300

    init create-react-library@3.0.6

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'table-options'
+import 'table-options/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
